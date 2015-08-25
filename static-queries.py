72e82b7a3c8d5646d75e51b64ecc4940c3e52b64
Simon Fowler <simon.fowler@anu.edu.au>
2015-08-25 11:47:54 +1000

    Stupid whitespace fixes.
    All tabs expanded, tabstop set to 8.
    
    Stupid stupid whitespace.

--- static-queries.py
+++ static-queries.py
@@ -8,129 +8,131 @@ from MySQLdb import cursors
 
 hypervisors_query="""
 select
-        id as hypervisor_id,
-        hypervisor_hostname as hostname,
-        host_ip as ip_address,
-        vcpus as cpus,
-        memory_mb as memory,
-        local_gb as local_storage
+	id as hypervisor_id,
+	hypervisor_hostname as hostname,
+	host_ip as ip_address,
+	vcpus as cpus,
+	memory_mb as memory,
+	local_gb as local_storage,
+	"NCI" as node
 from
-        nova.compute_nodes;
+	nova.compute_nodes;
 """
 projects_query="""
 select
-        distinct p.id as project_id,
-        p.name as display_name,
-        p.enabled as enabled,
-        i.hard_limit as instances,
-        c.hard_limit as cores,
-        r.hard_limit as ram,
-        g.total_limit as gigabytes,
-        v.total_limit as volumes,
-        s.total_limit as snapshots
+	distinct p.id as project_id,
+	p.name as display_name,
+	p.enabled as enabled,
+	i.hard_limit as instances,
+	c.hard_limit as cores,
+	r.hard_limit as ram,
+	g.total_limit as gigabytes,
+	v.total_limit as volumes,
+	s.total_limit as snapshots
 from
-        keystone.project as p left outer join
-        (
-        select  *  from  nova.quotas
-        where deleted = 0 and resource = 'ram'
-        ) as r on p.id = r.project_id left outer join
-        (
-        select  *  from  nova.quotas
-        where deleted = 0 and resource = 'instances'
-        ) as i on p.id = i.project_id left outer join
-        (
-        select  *  from  nova.quotas
-        where deleted = 0 and resource = 'cores'
-        ) as c on p.id = c.project_id left outer join
-        (
-        select
-                project_id,
-                sum(if(hard_limit>=0,hard_limit,0)) as total_limit
-        from
-                cinder.quotas
-        where deleted = 0 and resource like 'gigabytes%'
-        group by project_id
-        ) as g on p.id = g.project_id left outer join
-        (
-        select
-                project_id,
-                sum(if(hard_limit>=0,hard_limit,0)) as total_limit
-        from
-                cinder.quotas
-        where deleted = 0 and resource like 'volumes%'
-        group by project_id
-        ) as v on p.id = v.project_id left outer join
-        (
-        select
-                project_id,
-                sum(if(hard_limit>=0,hard_limit,0)) as total_limit
-        from
-                cinder.quotas
-        where deleted = 0 and resource like 'snapshots%'
-        group by project_id
-        ) as s on p.id = s.project_id;
+	keystone.project as p left outer join
+	(
+	select	*  from  nova.quotas
+	where deleted = 0 and resource = 'ram'
+	) as r on p.id = r.project_id left outer join
+	(
+	select	*  from  nova.quotas
+	where deleted = 0 and resource = 'instances'
+	) as i on p.id = i.project_id left outer join
+	(
+	select	*  from  nova.quotas
+	where deleted = 0 and resource = 'cores'
+	) as c on p.id = c.project_id left outer join
+	(
+	select
+		project_id,
+		sum(if(hard_limit>=0,hard_limit,0)) as total_limit
+	from
+		cinder.quotas
+	where deleted = 0 and resource like 'gigabytes%'
+	group by project_id
+	) as g on p.id = g.project_id left outer join
+	(
+	select
+		project_id,
+		sum(if(hard_limit>=0,hard_limit,0)) as total_limit
+	from
+		cinder.quotas
+	where deleted = 0 and resource like 'volumes%'
+	group by project_id
+	) as v on p.id = v.project_id left outer join
+	(
+	select
+		project_id,
+		sum(if(hard_limit>=0,hard_limit,0)) as total_limit
+	from
+		cinder.quotas
+	where deleted = 0 and resource like 'snapshots%'
+	group by project_id
+	) as s on p.id = s.project_id;
 """
 # note that this is a real pain, since "flavourid" is not unique, but it's the
 # term that's used elsewhere
 flavours_query = """
 select
-        id as _flavour_id,
-        flavorid as flavour_id,
-        name,
-        vcpus,
-        memory_mb as memory,
-        root_gb as root,
-        ephemeral_gb as ephemeral,
-        is_public as public
+	id as _flavour_id,
+	flavorid as flavour_id,
+	name,
+	vcpus,
+	memory_mb as memory,
+	root_gb as root,
+	ephemeral_gb as ephemeral,
+	is_public as public
 from
-        nova.instance_types;
+	nova.instance_types;
 """
 instances_query = """
 select
-        project_id,
-        uuid as instance_id,
-        display_name as name,
-        vcpus,
-        memory_mb as memory,
-        root_gb as root,
-        ephemeral_gb as ephemeral,
-        instance_type_id as flavour,
-        unix_timestamp(created_at) as created,
-        unix_timestamp(deleted_at) as deleted,
-        unix_timestamp(ifnull(deleted_at,now()))-unix_timestamp(created_at) as allocation_time,
-        0 as wall_time,
-        0 as cpu_time,
-        if(deleted<>0,false,true) as active,
+	project_id,
+	uuid as instance_id,
+	display_name as name,
+	vcpus,
+	memory_mb as memory,
+	root_gb as root,
+	ephemeral_gb as ephemeral,
+	instance_type_id as flavour,
+	unix_timestamp(created_at) as created,
+	unix_timestamp(deleted_at) as deleted,
+	unix_timestamp(ifnull(deleted_at,now()))-unix_timestamp(created_at) as allocation_time,
+	0 as wall_time,
+	0 as cpu_time,
+	if(deleted<>0,false,true) as active,
+	host as hypervisor,
 	ifnull(availability_zone, '') as availability_zone
 from
-        nova.instances;
+	nova.instances;
 """
 volumes_query = """
 select
-        id as volume_id,
-        project_id,
-        display_name,
+	id as volume_id,
+	project_id,
+	display_name,
 	size,
-        unix_timestamp(created_at) as created,
-        unix_timestamp(deleted_at) as deleted,
-        if(attach_status='attached',true,false) as attached,
-        instance_uuid,
+	unix_timestamp(created_at) as created,
+	unix_timestamp(deleted_at) as deleted,
+	if(attach_status='attached',true,false) as attached,
+	instance_uuid,
 	ifnull(availability_zone, '') as availability_zone
 from
-        cinder.volumes;
+	cinder.volumes;
 """
 images_query = """
 select
-        id as image_id,
-        owner as project_id,
-        name,
-        size,
-        status,
-        is_public as public,
-        unix_timestamp(created_at) as created,
-        unix_timestamp(deleted_at) as deleted
+	id as image_id,
+	owner as project_id,
+	name,
+	size,
+	status,
+	is_public as public,
+	unix_timestamp(created_at) as created,
+	unix_timestamp(deleted_at) as deleted
 from
-        glance.images;
+	glance.images;
 """
 
 # This data structure specifes the series name, the query to fill it out, and a list of tags and the source fields in the database results
@@ -153,18 +155,21 @@ from
 # For the initial implementation this will do . . .
 static_series = {
 	'hypervisors': {
+		'database': 'nova',
 		'query': hypervisors_query,
 		'tags': {
 			'node': ""
 		}
 	},
 	'projects': {
+		'database': 'keystone',
 		'query': projects_query,
 		'tags': {
 			'project_id': 'project_id'
 		}
 	},
 	'instances': {
+		'database': 'nova',
 		'query': instances_query,
 		'tags': {
 			'instance_id': 'instance_id',
@@ -177,12 +182,14 @@ static_series = {
 		},
 	},
 	'flavours': {
+		'database': 'nova',
 		'query': flavours_query,
 		'tags': {
 			'flavour_id': 'flavour_id'
 		}
 	},
 	'images': {
+		'database': 'glance',
 		'query': images_query,
 		'tags': {
 			'image_id': 'image_id',
@@ -190,6 +197,7 @@ static_series = {
 		}
 	},
 	'volumes': {
+		'database': 'cinder',
 		'query': volumes_query,
 		'tags': {
 			'volume_id': 'volume_id',
@@ -203,18 +211,18 @@ static_series = {
 # accodring to the series definitions here.
 
 def database_query(db, query):
-        """
-        Run a query against the database and return the result set
-        """
-        cursor = db.cursor(cursors.DictCursor)
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        return rows
+	"""
+	Run a query against the database and return the result set
+	"""
+	cursor = db.cursor(cursors.DictCursor)
+	cursor.execute(query)
+	rows = cursor.fetchall()
+	return rows
 
 def assemble_data_point(measurement, row, time=None):
-        """
-        Take a result row from a DB query and assemble them into a data set for influxdb
-        """
+	"""
+	Take a result row from a DB query and assemble them into a data set for influxdb
+	"""
 	# The data is in the form of dict containing a measurement field, a tags dict,
 	# a fields dict, and an optional time field. When the time isn't specified the
 	# data point will get the current time as seen by the server.
@@ -243,8 +251,8 @@ def assemble_data_points(measurement, rows, time=None):
 	return tmp
 
 for k in static_series.keys():
-        for j in static_series[k].keys():
-                print k, j
+	for j in static_series[k].keys():
+		print k, j
 
 
 db = MySQLdb.connect(host='localhost', user='reporting-update', passwd='needs to be set', db='reporting')
@@ -256,9 +264,6 @@ for k in static_series.keys():
 	print k, len(data)
 	points = assemble_data_points(k, data)
 	print len(points)
-#"	pprint.pprint(points)
 
 	print "Writing %d points to database" % (len(points))
 	idb.write_points(points=points, batch_size=1000)
-
-
